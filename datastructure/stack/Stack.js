cf39a7290186545e348fa9d594ebf540448f1861
sminha <sminha8590@naver.com>
2024-10-31 11:10:17 +0900

    docs: Remove semicolon

--- datastructure/stack/Stack.js
+++ datastructure/stack/Stack.js
@@ -2,38 +2,38 @@ import LinkedList from '../linked-list/LinkedList.js'
 
 export default class Stack {
   constructor() {
-    this.linkedList = new LinkedList();
+    this.linkedList = new LinkedList()
   }
 
   isEmpty() {
-    return !this.linkedList.head;
+    return !this.linkedList.head
   }
 
   peek() {
     if (this.isEmpty()) {
-      return null;
+      return null
     }
 
-    return this.linkedList.head.value;
+    return this.linkedList.head.value
   }
 
   push(value) {
-    this.linkedList.prepend(value);
+    this.linkedList.prepend(value)
   }
 
   pop() {
-    const removedHead = this.linkedList.deleteHead();
+    const removedHead = this.linkedList.deleteHead()
 
-    return removedHead ? removedHead.value : null;
+    return removedHead ? removedHead.value : null
   }
 
   toArray() {
     return this.linkedList
       .toArray()
-      .map((linkedListNode) => linkedListNode.value);
+      .map((linkedListNode) => linkedListNode.value)
   }
 
   toString(callback) {
-    return this.linkedList.toString(callback);
+    return this.linkedList.toString(callback)
   }
 }
